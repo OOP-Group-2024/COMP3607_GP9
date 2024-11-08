73b96334cbe94d8bfa86276b6e6daaf9b4594dbd
quanafoon <quanafoon@gmail.com>
2024-11-08 04:26:42 -0400

    Made TestGroup for ChatBotPlatform

---
+++ autograder/src/main/java/comp3607/ChatBotPlatformTest.java
@@ -0,0 +1,31 @@
+package comp3607;
+
+import java.util.List;
+
+public class ChatBotPlatformTest {
+    
+    TestGroup chatBotPlatform = new TestGroup();
+
+    public void setUp() {
+
+
+        VariableCriteria botsCritera = new VariableCriteria("private", "ArrayList<ChatBot>");
+        VariableTest bots = new VariableTest("bots", botsCritera);
+        chatBotPlatform.addTest(bots);
+
+        ConstructorTest constructorTest = new ConstructorTest(List.of(), List.of());
+        chatBotPlatform.addTest(constructorTest);
+
+        MethodCriteria addChatBotCriteria = new MethodCriteria( "public", "boolean", List.of(int.class));
+        MethodTest addChatBot = new MethodTest("addChatBot", addChatBotCriteria);
+        chatBotPlatform.addTest(addChatBot);
+
+        MethodCriteria getChatBotListCriteria = new MethodCriteria("public", "String", List.of());
+        MethodTest getChatBotList = new MethodTest("getChatBotList", getChatBotListCriteria);
+        chatBotPlatform.addTest(getChatBotList);
+
+        MethodCriteria InteractWithBotCriteria = new MethodCriteria("public", "String", List.of(int.class, String.class));
+        MethodTest InteractWithBot = new MethodTest("InteractWithBot", InteractWithBotCriteria);
+        chatBotPlatform.addTest(InteractWithBot);
+    }
+}
