55bc85769777ed7c0b28820e6ffe52d4e895be2e
Jakob Young Cassim <jyc.3718@gmail.com>
2024-11-17 21:34:35 -0400

    Removed commented out code

--- autograder/src/main/java/comp3607/App.java
+++ autograder/src/main/java/comp3607/App.java
@@ -5,17 +5,6 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        // System.out.println("Testing:\n");
-
-            // FileTest[] tests = { new ChatBotTest(), new ChatBotPlatformTest(), new ChatBotGeneratorTest(), new SimulationTest()};
-            // FileContext context = new FileContext();
-            // Report report = new Report();
-            // for (FileTest test : tests){
-            //     context.setTest(test);
-            //     context.testFile(report);
-            // }
-            // System.out.println(report.generateReport());
-
 
             // Step 1: Ensure the folder exists
   
